b70ee7179ad58a73a482a80c8f0e9af1447352ea
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-03-19 16:16:43 -0300

    feat: utils package for text styles

---
+++ lib/app/core/utils/text_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle h1 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h2Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h3Bold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle p = TextStyle(
+    fontSize: 20,
+  );
+
+  static const TextStyle pBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 16,
+  );
+}
