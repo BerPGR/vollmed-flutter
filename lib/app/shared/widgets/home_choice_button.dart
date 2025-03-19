b1a781a7c610c56ab48da2754b729be1e588e35b
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-03-19 16:17:32 -0300

    feat: created widget for multiple choices in homepage

---
+++ lib/app/shared/widgets/home_choice_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:vollmed/app/core/utils/text_styles.dart';
+
+class HomeChoiceButton extends StatelessWidget {
+  final String title;
+  final String image;
+  const HomeChoiceButton({super.key, required this.title, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff0B3B60),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        height: 140,
+        width: double.infinity,
+        child: InkWell(
+          onTap: () {
+            print("Clicou");
+          },
+          splashColor: Colors.white10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 10,
+            children: [
+              Image.asset(image),
+              Text(
+                title,
+                style: TextStyles.h3Bold.merge(TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
